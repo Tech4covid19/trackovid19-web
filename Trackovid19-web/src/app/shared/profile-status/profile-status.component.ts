aadb49147e21043421bf817b43a44125ea082b5f
Fabio Silva <fabio.silva@blip.pt>
2020-03-16 22:06:51 +0000

    Add main screen (location-status, profile-status status-btn and profile)

---
+++ Trackovid19-web/src/app/shared/profile-status/profile-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-status',
+  templateUrl: './profile-status.component.html',
+  styleUrls: ['./profile-status.component.scss']
+})
+export class ProfileStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
