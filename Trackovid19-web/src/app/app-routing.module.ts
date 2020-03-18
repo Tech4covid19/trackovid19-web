57cb3dbd8a6059653024be1b9834684c0c904f9c
Kevin Amorim <kevinamorim4@gmail.com>
2020-03-18 01:08:39 +0000

    Step 2 and 3 - Views

--- Trackovid19-web/src/app/app-routing.module.ts
+++ Trackovid19-web/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './screens/home/home.component';
 import { LoginComponent } from './screens/login/login.component';
-import { ChangeStateComponent } from './screens/change-state/change-state.component';
 import { MainComponent } from './screens/main/main.component';
 import { PostCodeComponent } from './screens/post-code/post-code.component';
 import { ChangeStateStep1Component } from './screens/change-state-step1/change-state-step1.component';
+import { ChangeStateStep2Component } from './screens/change-state-step2/change-state-step2.component';
+import { ChangeStateStep3Component } from './screens/change-state-step3/change-state-step3.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -20,9 +21,11 @@ const routes: Routes = [
       },
     ],
   },
-  {path: 'change-state', component: ChangeStateComponent},
   {path: 'change-state-step1', component: ChangeStateStep1Component},
+  {path: 'change-state-step2', component: ChangeStateStep2Component},
+  {path: 'change-state-step3', component: ChangeStateStep3Component},
   {path: 'post-code', component: PostCodeComponent}
+
 
 ];
 
