bb17c325b4f12a12075472a23508dc87e4b92ee5
Fabio <fabioazevedo0619@gmail.com>
2020-03-19 19:19:30 +0000

    Conditions now are loaded via api

--- Trackovid19-web/src/app/screens/change-state-step2/change-state-step2.component.ts
+++ Trackovid19-web/src/app/screens/change-state-step2/change-state-step2.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ConditionService } from 'src/app/states/condition/state/condition.service';
+import { Condition } from 'src/app/states/condition/state/condition.model';
 
 @Component({
   selector: 'app-change-state-step2',
   templateUrl: './change-state-step2.component.html',
-  styleUrls: ['./change-state-step2.component.scss']
+  styleUrls: ['./change-state-step2.component.scss'],
 })
 export class ChangeStateStep2Component implements OnInit {
+  conditions: Condition[];
 
-  constructor() { }
+  constructor(private conditionService: ConditionService) {}
 
   ngOnInit(): void {
+    this.conditionService.get().subscribe(conditions => {
+      this.conditions = conditions;
+    });
   }
-
 }
