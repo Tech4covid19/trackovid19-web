9d1b59be1a689ff825456afed46afd19eaf1ee1e
Luis Borges <lborges@codavel.com>
2020-03-20 18:37:57 +0000

    test prod env

--- Trackovid19-web/src/environments/environment.ts
+++ Trackovid19-web/src/environments/environment.ts
@@ -6,7 +6,7 @@
 const SERVER_URL = 'https://op2pwi0nd1.execute-api.eu-west-1.amazonaws.com/prd';
 
 export const environment = {
-  production: false,
+  production: true,
   serverURL: SERVER_URL,
   apiUrl: SERVER_URL + '/api/v1/',
 };
