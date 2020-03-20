202a24c0ab981190d54b477882c825013d309bcc
Fabio Silva <fabio.silva@blip.pt>
2020-03-20 17:44:09 +0000

    fix mapping of colors and icons with backend codes

--- Trackovid19-web/src/app/shared/pipes/icon-placeholder.pipe.ts
+++ Trackovid19-web/src/app/shared/pipes/icon-placeholder.pipe.ts
@@ -1,23 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'iconPlaceholder'
+  name: 'iconPlaceholder',
 })
 export class IconPlaceholderPipe implements PipeTransform {
-
   transform(value: unknown, ...args: unknown[]): unknown {
-    switch(value) {
-      case 4:
-        return 'assets/img/illustrations/dashboard/heart.svg';
-      case 2:
-        return 'assets/img/illustrations/dashboard/user-orange.svg'
-      case 5:
-        return 'assets/img/illustrations/dashboard/user-red.svg'
-      case 3:
-        return 'assets/img/illustrations/dashboard/green.svg'
+    switch (value) {
       case 1:
-        return 'assets/img/illustrations/dashboard/disease.svg'
+        return 'assets/img/illustrations/dashboard/disease.svg';
+      case 2:
+        return 'assets/img/illustrations/dashboard/user-red.svg';
+      case 3:
+        return 'assets/img/illustrations/dashboard/green.svg';
+      case 4: // TO FIX:
+        return 'assets/img/illustrations/dashboard/heart.svg';
+      case 100:
+        return 'assets/img/illustrations/dashboard/user-orange.svg';
+      case 200:
+        return 'assets/img/illustrations/dashboard/heart.svg';
     }
   }
-
 }
