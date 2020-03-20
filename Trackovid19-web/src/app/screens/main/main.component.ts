d17b2e7035cdbb4a5ca366f00347393a6a5f7c35
Walter <waltergandarella@yesmkt.com>
2020-03-20 12:48:58 +0000

    issue #75

--- Trackovid19-web/src/app/screens/main/main.component.ts
+++ Trackovid19-web/src/app/screens/main/main.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from 'src/app/states/user/state/user.service';
+import { ConfinementStateService } from '../../states/confinement-state/state/confinement-state.service';
+import { User } from '../../states/user/state/user.model';
+import { ConfinementState } from '../../states/confinement-state/state/confinement-state.model';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-  constructor(private userService: UserService) {}
+export class MainComponent implements OnInit, OnDestroy {
+  user: User = null;
+  confinementState: ConfinementState = null;
+
+  private subs = new SubSink();
+
+  constructor(
+    private userService: UserService,
+    private conditionService: ConfinementStateService,
+  ) {}
 
   ngOnInit(): void {
-    this.userService.getUser().subscribe();
+    this.loadUser();
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+  private loadUser() {
+    this.subs.add(
+      this.userService.getUser().subscribe(user => {
+        this.user = { ...user };
+        this.loadState();
+      }),
+    );
+  }
+
+  private loadState() {
+    this.subs.add(
+      this.conditionService.get().subscribe((states: any[]) => {
+        this.confinementState =
+          states.find(state => state.id === +this.user.confinement_state) || null;
+      }),
+    );
   }
 }
