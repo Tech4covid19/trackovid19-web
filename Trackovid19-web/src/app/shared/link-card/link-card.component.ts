da6b2b1348cbac2997fd0328f3b56c23e17e9730
Diogo Teixeira <diogo.teixeira@blip.pt>
2020-03-20 14:31:21 +0000

    terms and conditions screen

---
+++ Trackovid19-web/src/app/shared/link-card/link-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-link-card',
+  templateUrl: './link-card.component.html',
+  styleUrls: ['./link-card.component.scss'],
+})
+export class LinkCardComponent {
+  @Input() label: string;
+  @Input() url: string;
+
+  constructor(public router: Router) {}
+
+  public goToPage(url: string) {
+    this.router.navigate([]).then(result => {
+      window.open(url, '_blank');
+    });
+  }
+}
