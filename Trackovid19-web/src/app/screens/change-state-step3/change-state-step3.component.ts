feeadb31e960c8d47fecacbfccc891a68094c84c
Fabio <fabioazevedo0619@gmail.com>
2020-03-20 11:26:26 +0000

    After change-state 1,2,3 a call is made to api to save the case

--- Trackovid19-web/src/app/screens/change-state-step3/change-state-step3.component.ts
+++ Trackovid19-web/src/app/screens/change-state-step3/change-state-step3.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ConfinementStateService } from 'src/app/states/confinement-state/state/confinement-state.service';
 import { ConfinementState } from 'src/app/states/confinement-state/state/confinement-state.model';
 import { Step } from 'src/app/shared/steps/steps.component';
+import { Case } from 'src/app/states/case/state/case.model';
+import { CaseService } from 'src/app/states/case/state/case.service';
 
 @Component({
   selector: 'app-change-state-step3',
@@ -16,22 +18,41 @@ export class ChangeStateStep3Component implements OnInit {
     { label: '2', url: 'change-state-step2', active: true },
     { label: '3', url: 'change-state-step3', active: true },
   ];
+  case: Case;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private confinementStateService: ConfinementStateService,
+    private caseService: CaseService,
   ) {}
 
   ngOnInit(): void {
+    this.case = history.state.data;
+
     this.confinementStateService.get().subscribe(confinementStates => {
       this.confinementStates = confinementStates;
     });
   }
 
-  sendForm() {
-    this.router.navigate(['/dashboard', { outlets: { dash: ['status'] } }], {
-      relativeTo: this.activatedRoute,
-    });
+  updateConfinementState(confinementStateId: number, $event): void {
+    this.case.confinementState = undefined;
+
+    if ($event.target.checked) {
+      this.case.confinementState = confinementStateId;
+    }
+  }
+
+  sendForm(): void {
+    this.caseService.add(this.case).subscribe(
+      () => {
+        this.router.navigate(['/dashboard', { outlets: { dash: ['status'] } }], {
+          relativeTo: this.activatedRoute,
+        });
+      },
+      err => {
+        console.log(err);
+      },
+    );
   }
 }
