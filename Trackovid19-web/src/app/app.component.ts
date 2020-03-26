c29deeab25dea3b06180a0a22cc51dfb13a1f368
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-03-26 10:42:45 +0000

    fix facebook

--- Trackovid19-web/src/app/app.component.ts
+++ Trackovid19-web/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
 import { GeolocalizationService } from './shared/services/geolocalization.service';
 import { environment } from '../environments/environment';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,23 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
   title = 'Trackovid19-web';
-  constructor(geolocalizationService: GeolocalizationService) {
+  constructor(
+    geolocalizationService: GeolocalizationService,
+    private renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document,
+  ) {
     //geolocalizationService.geoFindMe()
 
     // Init Facebook SDK
-    // @ts-ignore
-    window.fbAsyncInit = () => {
-      // @ts-ignore
+    const s = this.renderer2.createElement('script');
+    s.text = `window.fbAsyncInit = () => {
       FB.init({
-        appId: environment.facebookAPI,
+        appId: ${environment.facebookAPI},
         autoLogAppEvents: true,
         xfbml: true,
         version: 'v6.0',
       });
-    };
+    };`;
+    this.renderer2.appendChild(this._document.body, s);
   }
 }
