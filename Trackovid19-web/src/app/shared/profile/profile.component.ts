fab27a8f8865cef641fbfc8cca20714c2a41aee6
Fabio Silva <fabio.silva@blip.pt>
2020-03-26 19:04:33 +0000

    add notifications draft

--- Trackovid19-web/src/app/shared/profile/profile.component.ts
+++ Trackovid19-web/src/app/shared/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { User } from '../../states/user/state/user.model';
+import { SwPush } from '@angular/service-worker';
+import { NotificationService } from '../services/notification-service.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -9,9 +12,37 @@ import { User } from '../../states/user/state/user.model';
 export class ProfileComponent implements OnChanges {
   @Input() user: User = null;
 
-  constructor() {}
+  sub: PushSubscription;
+
+  readonly VAPID_PUBLIC_KEY = environment.serverPublicKey;
+
+  constructor(private swPush: SwPush, private notificationService: NotificationService) {}
 
   ngOnChanges(): void {
     // TODO
+  }
+
+  subscribeToNotifications() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then(sub => {
+        this.sub = sub;
+
+        console.log('Notification Subscription: ', sub);
+
+        this.notificationService.addPushSubscriber(sub).subscribe(
+          () => console.log('Sent push subscription object to server.'),
+          err => console.log('Could not send subscription object to server, reason: ', err),
+        );
+      })
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
+
+  sendNewsletter() {
+    console.log('Sending Newsletter to all Subscribers ...');
+
+    this.notificationService.send().subscribe();
   }
 }
