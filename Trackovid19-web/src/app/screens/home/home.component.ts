a400a7f60960d60cd40222274e2210f448d3485a
Fabio Silva <fabio.silva@blip.pt>
2020-03-31 21:44:00 +0100

    remove duplicated user call

--- Trackovid19-web/src/app/screens/home/home.component.ts
+++ Trackovid19-web/src/app/screens/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/states/user/state/user.service';
-import { GeolocalizationService } from 'src/app/shared/services/geolocalization.service';
 import { Router } from '@angular/router';
 import { DashboardService } from 'src/app/states/dashboard/dashboard.service';
 import { UserQuery } from 'src/app/states/user/state/user.query';
+import { User } from 'src/app/states/user/state/user.model';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +12,7 @@ import { UserQuery } from 'src/app/states/user/state/user.query';
 export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
-    private userService: UserService,
     private userQuery: UserQuery,
-    private localizeService: GeolocalizationService,
     private dashboardService: DashboardService,
   ) {}
 
@@ -30,10 +27,11 @@ export class HomeComponent implements OnInit {
   }
 
   verifyCode(): void {
-    this.userService.getUser().subscribe(user => {
-      if (user && user.postalcode && user.postalcode === '0000-000') {
-        this.router.navigate(['post-code-step']);
-      }
-    });
+    const id = this.userQuery.getActiveId();
+    const user: User = this.userQuery.getEntity(id);
+
+    if (user?.postalcode && user.postalcode === '0000-000') {
+      this.router.navigate(['post-code-step']);
+    }
   }
 }
