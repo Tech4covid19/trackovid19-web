0f096260755b9690b435128e1cc69ba2a4f2d7a5
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-03-31 17:20:45 +0100

    remove duplicate service calls

--- Trackovid19-web/src/app/screens/home/home.component.ts
+++ Trackovid19-web/src/app/screens/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/states/user/state/user.service';
 import { GeolocalizationService } from 'src/app/shared/services/geolocalization.service';
 import { Router } from '@angular/router';
+import { DashboardService } from 'src/app/states/dashboard/dashboard.service';
+import { UserQuery } from 'src/app/states/user/state/user.query';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +14,19 @@ export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private userService: UserService,
+    private userQuery: UserQuery,
     private localizeService: GeolocalizationService,
+    private dashboardService: DashboardService,
   ) {}
 
   ngOnInit(): void {
     //this.localizeService.geoFindMe();
     this.verifyCode();
+
+    const id = this.userQuery.getActiveId();
+    const user = this.userQuery.getEntity(id);
+    this.dashboardService.getCasesByPostalCodeConditions(user.postalcode).subscribe(res => {});
+    this.dashboardService.getCasesByPostalCodeConfinements(user.postalcode).subscribe(res => {});
   }
 
   verifyCode(): void {
--- Trackovid19-web/src/app/shared/location-info/location-info.component.ts
+++ Trackovid19-web/src/app/shared/location-info/location-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardQuery } from 'src/app/states/dashboard/dashboard.query';
-import { DashboardService } from 'src/app/states/dashboard/dashboard.service';
 import { Observable } from 'rxjs';
 import { UserQuery } from 'src/app/states/user/state/user.query';
 import { Router } from '@angular/router';
@@ -11,24 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./location-info.component.scss'],
 })
 export class LocationInfoComponent implements OnInit {
-  count: number;
   postalCode$: Observable<any>;
   county$: Observable<any>;
-  constructor(
-    private query: DashboardQuery,
-    private userQuery: UserQuery,
-    private service: DashboardService,
-    private router: Router,
-  ) {}
+  count$: Observable<number>;
+
+  constructor(private query: DashboardQuery, private userQuery: UserQuery, private router: Router) {
+    this.count$ = this.query.selectFirst(state =>
+      state.conditions.reduce((total, d) => total + parseInt(d.hits, 10), 0),
+    );
+  }
 
   ngOnInit(): void {
-    this.count = 0;
-
     const id = this.userQuery.getActiveId();
     const user = this.userQuery.getEntity(id);
-    this.service.getCasesByPostalCode(user.postalcode).subscribe(res => {
-      this.count = res[1].reduce((total, d) => total + parseInt(d.hits, 10), 0);
-    });
     this.postalCode$ = this.userQuery.selectActive(entity => {
       return entity.postalcode.slice(0, -4); // Get just the first 4 digits
     });
