1360cbaf90d9de6d627577bbb07c6ba67977c905
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-04-07 23:14:04 +0100

    share location status page

--- Trackovid19-web/src/app/shared/share-location-status/share-location-status.component.ts
+++ Trackovid19-web/src/app/shared/share-location-status/share-location-status.component.ts
@@ -11,6 +11,7 @@ export class ShareLocationStatusComponent implements OnInit {
   @Input() showModal: boolean;
   @Input() toggleModal: Function;
   @Input() imageUrl: string;
+  @Input() postCode: string;
 
   closable = true;
 
@@ -20,6 +21,11 @@ export class ShareLocationStatusComponent implements OnInit {
     this.closeCallback = this.close.bind(this);
   }
 
+  shareText =
+    'Este é o estado da minha zona, como está a tua? Vê o estado da tua zona em covidografia.pt';
+
+  hashtags = ['AjudarQuemNosAjuda', 'covidografia', 'Tech4Covid19'];
+
   ngOnInit(): void {}
 
   public close() {
@@ -27,26 +33,32 @@ export class ShareLocationStatusComponent implements OnInit {
   }
 
   shareFacebook() {
-    this.shareHelper.facebook(this.imageUrl);
+    this.shareHelper.facebook(this.getImageUrl(), this.shareText, this.hashtags[0]);
   }
 
   shareWhatsapp() {
-    this.shareHelper.whatsapp(this.imageUrl);
+    this.shareHelper.whatsapp(`${this.shareText} ${this.getImageUrl()}`);
   }
 
   shareFacebookMessenger() {
-    this.shareHelper.facebookMessenger(this.imageUrl);
+    this.shareHelper.facebookMessenger(`${this.shareText} ${this.getImageUrl()}`);
   }
 
   shareLinkedIn() {
-    this.shareHelper.linkedIn(this.imageUrl);
+    this.shareHelper.linkedIn(this.getImageUrl());
   }
 
   shareTwitter() {
-    this.shareHelper.twitter(this.imageUrl);
+    this.shareHelper.twitter(this.getImageUrl(), this.hashtags);
   }
 
   shareCopyLinkText() {
-    this.shareHelper.copyText(this.imageUrl);
+    this.shareHelper.copyText(this.getImageUrl());
+  }
+
+  getImageUrl() {
+    const imageUrlSplit = this.imageUrl.split('.');
+    const imageId = imageUrlSplit[imageUrlSplit.length - 2].split('/').pop();
+    return `https://covidografia.pt/o-meu-codigo-postal/${this.postCode}/${imageId}`;
   }
 }
